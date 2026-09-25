8a084b59a8735072ef54411ed7d749f659cfeb23
MadefromLight <142700816+MadefromLight@users.noreply.github.com>
2026-09-24 20:05:20 -0700

    feat: add src/utils/jwt.ts

---
+++ src/utils/jwt.ts
@@ -0,0 +1,5 @@
+import jwt from "jsonwebtoken";
+import { env } from "../config/env";
+export function signToken(user:{id:string;role:"CUSTOMER"|"ADMIN"}) {
+ return jwt.sign(user,env.JWT_SECRET,{expiresIn:env.JWT_EXPIRES_IN});
+}
